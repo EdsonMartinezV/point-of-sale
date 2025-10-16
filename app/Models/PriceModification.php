3a0cdc67ae311c21567891a119d22db9d900d9be
edson <edsonjmv@outlook.com>
2025-10-16 15:40:11 -0600

    PriceModification model and stuff

---
+++ app/Models/PriceModification.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class PriceModification extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'sold_separetely',
+        'cost_price',
+        'first_wholesale_percentage',
+        'second_wholesale_percentage',
+        'third_wholesale_percentage',
+        'retail_percentage',
+        'product_id',
+        'purchase_item_id',
+    ];
+
+    public function product(): BelongsTo{
+        return $this->belongsTo(Product::class);
+    }
+
+    public function purchaseItem(): BelongsTo{
+        return $this->belongsTo(PurchaseItem::class);
+    }
+
+    public function saleItems(): HasMany{
+        return $this->hasMany(SaleItem::class);
+    }
+}
---
+++ database/migrations/2025_10_15_195535_create_price_modifications_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('price_modifications', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('sold_separetely')->default(false);
+            $table->decimal('cost_price', 10, 2)->nullable();
+            $table->decimal('first_wholesale_percentage', 0, 2)->nullable();
+            $table->decimal('second_wholesale_percentage', 0, 2)->nullable();
+            $table->decimal('third_wholesale_percentage', 0, 2)->nullable();
+            $table->decimal('retail_percentage', 0, 2)->nullable();
+            $table->foreignId('product_id')
+                ->constrained()
+                ->onUpdate('cascade');
+            $table->foreignId('purchase_item_id')
+                ->constrained()
+                ->onUpdate('cascade');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('price_modifications');
+    }
+};
