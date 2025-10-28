a7d51671e1de76a30ead74e879c40fbe67f0b5c6
edson <edsonjmv@outlook.com>
2025-10-28 16:02:23 -0600

    Quick fix

--- app/Http/Requests/StoreCategoryRequest.php
+++ app/Http/Requests/StoreCategoryRequest.php
@@ -6,13 +6,11 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class StoreCategoryRequest extends FormRequest
 {
-    public function authorize(): bool
-    {
+    public function authorize(): bool {
         return true;
     }
 
-    public function rules(): array
-    {
+    public function rules(): array {
         return [
             'name' => ['required', 'string', 'max:255'],
             'description' => ['nullable', 'string'],
