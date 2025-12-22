44cd84ed49bb5ef19ceaffa12c97b08a4564a768
edson <edsonjmv@outlook.com>
2025-12-22 16:33:15 -0600

    AppSidebar menu updated

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import NavFooter from '@/components/NavFooter.vue';
+/* import NavFooter from '@/components/NavFooter.vue'; */
 import NavMain from '@/components/NavMain.vue';
 import NavUser from '@/components/NavUser.vue';
 import {
@@ -11,14 +11,18 @@ import {
     SidebarMenuButton,
     SidebarMenuItem,
 } from '@/components/ui/sidebar';
-import { dashboard } from '@/routes';
+/* import { dashboard } from '@/routes'; */
 import { type NavItem } from '@/types';
 import { Link } from '@inertiajs/vue3';
-import { BookOpen, Folder, LayoutGrid, BaggageClaim, ShoppingBasket } from 'lucide-vue-next';
+import { /*BookOpen, Folder, LayoutGrid, */ BaggageClaim, ShoppingBasket, ContactRound, ListTree, Milk, Weight } from 'lucide-vue-next';
 import AppLogo from './AppLogo.vue';
 
 import PurchaseController from '@/actions/App/Http/Controllers/PurchaseController';
 import SaleController from '@/actions/App/Http/Controllers/SaleController';
+import ProviderController from '@/actions/App/Http/Controllers/ProviderController';
+import CategoryController from '@/actions/App/Http/Controllers/CategoryController';
+import ProductController from '@/actions/App/Http/Controllers/ProductController';
+import MeasureUnitController from '@/actions/App/Http/Controllers/MeasureUnitController';
 
 const mainNavItems: NavItem[] = [
     {
@@ -31,9 +35,29 @@ const mainNavItems: NavItem[] = [
         href: SaleController.index(),
         icon: ShoppingBasket,
     },
+    {
+        title: 'Proveedores',
+        href: ProviderController.index(),
+        icon: ContactRound,
+    },
+    {
+        title: 'Categorías',
+        href: CategoryController.index(),
+        icon: ListTree,
+    },
+    {
+        title: 'Productos',
+        href: ProductController.index(),
+        icon: Milk,
+    },
+    {
+        title: 'Unidades de medida',
+        href: MeasureUnitController.index(),
+        icon: Weight,
+    },
 ];
 
-const footerNavItems: NavItem[] = [
+/* const footerNavItems: NavItem[] = [
     {
         title: 'Github Repo',
         href: 'https://github.com/laravel/vue-starter-kit',
@@ -44,7 +68,7 @@ const footerNavItems: NavItem[] = [
         href: 'https://laravel.com/docs/starter-kits#vue',
         icon: BookOpen,
     },
-];
+]; */
 </script>
 
 <template>
