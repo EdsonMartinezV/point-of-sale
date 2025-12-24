5f72143b17e65eb246bd1560ac0d8f539f834310
edson <edsonjmv@outlook.com>
2025-12-24 08:16:58 -0600

    ts model types

--- resources/js/types/main.d.ts
+++ resources/js/types/main.d.ts
@@ -6,3 +6,32 @@ export interface Provider {
     address?: string;
     contact_person?: string;
 };
+
+export interface Category {
+    id: number;
+    name: string;
+    description?: string;
+}
+
+export interface Product {
+    id: number;
+    name: string;
+    sold_by_retail: boolean;
+    retail_units_per_box?: number;
+    stock: number;
+    retail_remaining_stock?: number;
+    cost_price?: number;
+    first_wholesale_percentage: number;
+    second_wholesale_percentage: number;
+    third_wholesale_percentage: number;
+    retail_percentage?: number;
+    category_id: number;
+    measure_unit_id: number;
+    retail_measure_unit_id: number;
+}
+
+export interface MeasureUnit {
+    id: number;
+    name: string;
+    abbreviation: string;
+}
