697b5b7b243027244eeb67c22309e1a0453cf542
edson <edsonjmv@outlook.com>
2026-04-04 21:01:45 -0600

    Skip ProductSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,7 +18,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             MeasureUnitSeeder::class,
             CategorySeeder::class,
-            ProductSeeder::class,
+            /* ProductSeeder::class, */
         ]);
     }
 }
